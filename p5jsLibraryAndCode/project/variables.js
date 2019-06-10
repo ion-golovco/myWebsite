d81ed1e10b07f064310a1729624022d814cdc514
ion-golovco <gaminglions2@gmail.com>
2019-06-10 12:13:26 +0300

    Add files via upload

--- p5jsLibraryAndCode/project/variables.js
+++ p5jsLibraryAndCode/project/variables.js
@@ -1,41 +1,72 @@
 //setup
-let attackSpeed = 0
-let button
-let name 
-let butto 
+let attackSpeed = 0;
+let button;
+let name;
+let butto;
 let fightB;
 //hits (up to 5)
 let hits = 0;
 //Story progresion
 let storyP = 0;
 //Start neccesary amount of xp to level up
-let limit = 60
+let limit = 60;
 //Start amount of hp
-let hp = 100
+let hp = 100;
 //How fast health regenerates
-let inc = 0.04
+let inc = 0.07;
 //Player name
 let pName;
 //Player
 let player;
 //test
-let index = 0
-let indez = 0
+let index = 0;
+let indez = 0;
 //Xp from killing enemies
-let reward = 0
+let reward = 0;
 //test
-let weapon = new Weapon(0, 6, "Branch")
-let enemy = new Enemy(3, randomName())
+let weapon = new Weapon(0, 6, "Branch");
+let enemy = new enemyDead();
+let shield = new Shield(0);
 //Key for game
-let accept = 0
+let accept = 0;
 //Race
-let race = randomRace()
+let race = randomRace();
 //Feed
-let feed = []
-let storyFeed = []
+let feed = [];
+let storyFeed = [
+    ["____________________________________________________________________________"],
+    ["I'ts an enemy"],
+    ["You hear a creak trough the dense foliage."],
+    ["you start your adventure by first going in the dark forest near your house."],
+    ["With nothing left than a branch you found outside,"]
+];
 //Equiped weapon
-let rare
-let slot
+let rare;
+let slot;
 
-let myFont
-let enemyKilled = 0
+let myFont;
+let enemyKilled = 0;
+
+let ex = 700;
+let ey = 125;
+
+let px = 400;
+
+let count = 0;
+let countm = 0;
+let active;
+let activeProj;
+
+let projx;
+
+let enGotHit = 125;
+let pGotHit = 125;
+let ered = 0;
+let pred = 0;
+
+let hit;
+let hurt;
+
+let for1 = []
+let for2 = []
+let arr = []
