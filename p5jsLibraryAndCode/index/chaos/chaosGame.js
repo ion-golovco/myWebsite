1a829dc43a95d47f293897037e1b1660629dce40
ion-golovco <gaminglions2@gmail.com>
2019-10-10 19:26:09 +0300

    Update chaosGame.js

--- p5jsLibraryAndCode/index/chaos/chaosGame.js
+++ p5jsLibraryAndCode/index/chaos/chaosGame.js
@@ -1,17 +1,17 @@
 //comenzi
     //background(0) sterge canvasul
 //maximul $per
-let maxi = 10
+let maxi = 4
 //puncte ale poligonului
-let pointL = 4
+let pointL = 3
 //doturi pentru fiecare iterare
-let sp = 600
+let sp = 10000
 //distanta la care sar doturile
-let per = 1
+let per = 1.998
 //incrementul de crestere a $per
-let inc = 0.001
+let inc = 0.001 
 //scale cu cit mai mare numarul cu cit devine mai mic poligonu
-let scale = 10000
+let scale = 500
 
 let points = []
 let total = 0
@@ -34,10 +34,8 @@ function setup() {
         v.z = i
         points.push(v)
     }
-    for(let i =0;i<pointL;i++){
-        colors.push([random(255),random(255),random(255)])
-    }
-    curent = createVector(random(w), random(h))
+    //for(let i =0;i<pointL;i++){colors.push([random(255),random(255),random(255)])}
+    curent = createVector(0, 0)
     frameRate(30)
     stroke(255)
     for (let i of points) { circle(i.x, i.y, 2) }
@@ -47,7 +45,7 @@ function draw() {
         per += inc
     }
     {
-        background(0,3)
+        //background(0)
         translate(w / 2, h / 2)
         strokeWeight(1.1)
     }
@@ -57,12 +55,12 @@ function draw() {
     }
     for (let i = 0; i < sp; i++) {
         let next = random(points)
-        for(let i in colors){
-            if(next.z == i){
-                stroke(colors[i],100)
+        //for(let i in colors){
+         //   if(next.z == i){
+        //        stroke(colors[i],100)
                 //stroke(255)
-            }
-        }
+        //    }
+       // }
        //if (next !== previous) { //pentagon1
             curent.x = lerp(curent.x, next.x, per)
             curent.y = lerp(curent.y, next.y, per)
